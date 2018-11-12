74e94739973b2c51846a371a1366f195bdcf6aa0
ouajjita-dcu <abdelkrim.quajjit2@mail.dcu.ie>
2018-11-12 23:20:05 +0000

    The final Assignment 2 application .Comments included.

--- app/src/main/java/abdelkrim/ouajjit/com/sda_assign2_abdelkrim_ouajjit/MainActivity.java
+++ app/src/main/java/abdelkrim/ouajjit/com/sda_assign2_abdelkrim_ouajjit/MainActivity.java
@@ -1,96 +1,127 @@
 package abdelkrim.ouajjit.com.sda_assign2_abdelkrim_ouajjit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.ref.Reference;
 
+public class MainActivity extends AppCompatActivity {
+    //Declaring variables of the type TextView.
     private TextView openCamera;
     private TextView viewPicture;
     private TextView explicitIntent;
     private TextView showMessage;
+    //Declaring variables of the type string.
     private String messageTo;
     private String messageSubject;
     private String messageCompose;
-
+    //Declaring a static final variable of the type integer.
     private static final int CAMERA_IMAGE_REQUEST_IMAGE_CODE=1;
-    private static final int REQUEST_CODE=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*
+         * Required call through to Activity onCreate().
+         * Restore any saved instance state.
+         */
         super.onCreate(savedInstanceState);
+        // Set up the application's user interface (Content View).
         setContentView(R.layout.activity_main);
-
-        openCamera =   findViewById(R.id.openCameraID);
-        viewPicture=   findViewById(R.id.viewPictureID);
+        // Get a reference to TextView in the content view,
+        openCamera = findViewById(R.id.openCameraID);
+        viewPicture= findViewById(R.id.viewPictureID);
         explicitIntent=findViewById(R.id.callAnActivityID);
-        showMessage =  findViewById(R.id.showMessageID);
-
+        showMessage = findViewById(R.id.showMessageID);
+        // set OnClickListener for TextView
         openCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+        // Called the dispatchTakePictureIntent() method when the user taps TextView openCamera.
                 dispatchTakePictureIntent();
+                Log.i("tag'","dispatchTakePictureIntent() called");
             }
         });
         viewPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+        // Called the viewThePicture() method when the user taps TextView viewPicture.
                 viewThePicture();
+                Log.i("tag'","viewThePicture() called");
             }
         });
         explicitIntent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+        // Called the createTheSecondActivity() method when the user taps TextView explicitIntent.
                 createTheSecondActivity();
+                Log.i("tag'","createTheSecondActivity() called");
             }
         });
 
 
         /*
-        * Extract the data from explicitActivity.
-        * Reference : https://developer.android.com/training/basics/firstapp/starting-activity#java.
-        * Reference : https://developer.android.com/reference/android/app/Activity.
-        */
+         * Reference : https://developer.android.com/training/basics/firstapp/starting-activity#java.
+         * Reference : https://developer.android.com/reference/android/app/Activity.
+         */
+
+        /*
+         * Get the current intent.
+         * Get the attached extras from the intent through the Bundle.
+         */
         Bundle extras = getIntent().getExtras();
-        if (extras!=null) {
+        if (extras!=null) { // If there is some data
+         // We should use the same key as we used to attach the data to retrieve the data.
             messageTo = extras.getString("TO");
             messageSubject = extras.getString("SUBJECT");
             messageCompose = extras.getString("COMPOSE");
+            // If all the fields are completed then we can pass all the information to set the Text View with our message.
             if (messageTo!=null && messageSubject!=null && messageCompose!=null) {
                 // Capture the layout's TextView and set the string as its text
                 showMessage.setText("To : " + messageTo + "\nSubject: \t" + messageSubject + "\nCompose: \t" + messageCompose);
             }
             }
-
-
     }
-    // invoking the dispatchTakePictureIntent() method when the user clicks a TextView openCamera.
-    // TODO: try only startActivity() method .
+
+    /*
+     * Reference : https://developer.android.com/guide/components/intents-common.
+     * Invoking the dispatchTakePictureIntent() method when the user clicks a TextView openCamera.
+     */
+
     private void dispatchTakePictureIntent() {
-        //Start a camera app in still image mode
-        //Reference : https://developer.android.com/guide/components/intents-common.
+        // Starting a camera app using intent filter STILL_IMAGE_CAMERA
         Intent takePictureIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+        /*
+         * Starting the startActivityForResult() method to receive a result back.
+         * The integer argument is a "CAMERA_IMAGE_REQUEST_IMAGE_CODE" that identifies your request.
+         */
+
             startActivityForResult(takePictureIntent,CAMERA_IMAGE_REQUEST_IMAGE_CODE);
+        // Toast message when the camera is launched.
             Toast.makeText(MainActivity.this, "camera is launched", Toast.LENGTH_LONG).show();
         }
     }
     // invoking the viewThePicture() method when the user clicks a TextView viewPicture.
     private void viewThePicture() {
-        //Reference : https://stackoverflow.com/questions/16928727/open-gallery-app-from-android-intent.
+
         /*
+         * Reference : https://stackoverflow.com/questions/16928727/open-gallery-app-from-android-intent.
          * Open the Android gallery application from an intent :
          * Invoke the image gallery using an implicit intent.
          * Creating an intent object called viewPictureIntent by using the key word new .
          */
+
         Intent viewPictureIntent = new Intent();
-        // creating an Intent with the action, ACTION_VIEW because we want to VIEW the contents ..
+        // Creating an Intent with the ACTION_VIEW because we want to VIEW the contents ..
         viewPictureIntent.setAction(Intent.ACTION_VIEW);
         // Set the data and type ,Get all images types
         viewPictureIntent.setType("image/*");
@@ -103,24 +134,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+        /*
+         * Reference : https://developer.android.com/training/basics/firstapp/starting-activity#java.
+         * Invoking the createTheSecondActivity() to create the second activity.
+         */
+
     private void createTheSecondActivity() {
-        //Create the second activity.
+
         //Reference : https://developer.android.com/training/basics/firstapp/starting-activity#java.
         Intent secondActivityIntent = new Intent(this,explicitActivity.class);
-
+        //starting the activity
         startActivity(secondActivityIntent);
     }
 
     public void sendEmail(View view){
-        /** Called when the user taps the Send button */
-
+        // Called when the user taps the Send button
+        // Get the Intent that started this activity and extract the string
             Intent sendIntent = new Intent();
+        //Set the intent action send
             sendIntent.setAction(Intent.ACTION_SEND);
             sendIntent.setType("plain/text");
             sendIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{messageTo});
             sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, messageSubject);
             sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, messageCompose);
         if (messageTo!=null && messageSubject!=null && messageCompose!=null){
+            //starting the activity
             startActivity(sendIntent);
         }else{
             Toast.makeText(getApplicationContext(), "No Data present " +
--- app/src/main/java/abdelkrim/ouajjit/com/sda_assign2_abdelkrim_ouajjit/explicitActivity.java
+++ app/src/main/java/abdelkrim/ouajjit/com/sda_assign2_abdelkrim_ouajjit/explicitActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class explicitActivity extends AppCompatActivity {
+    //Declaring variables of the type TextView.
 
     private EditText editTextTo;
     private EditText editTextSubject;
@@ -20,14 +21,15 @@ public class explicitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit);
     }
-    //TODO all the input must be valid ..done
-    //TODO Valid email input ...
+
+        // Reference : https://developer.android.com/reference/java/util/regex/Pattern.
     public void sendMessage(View view) {
+
         /*
-         * Called when the user taps the Send button
-         * The resulting pattern can then be used to create a Matcher object that can match arbitrary
-         * character sequences against the regular expression.
-         * Reference : https://developer.android.com/reference/java/util/regex/Pattern.
+         * Calling the sendMessage()method when the user taps the Send button
+         * We can start adding data into the Intent object, we use the method defined in the Intent class putExtra()
+         * Or putExtras() to store certain data as a key value pair or Bundle data object.
+         * These key-value pairs are known as Extras in the sense we are talking about Intents.
          *
          */
 
@@ -39,14 +41,29 @@ public class explicitActivity extends AppCompatActivity {
         String messageTo = editTextTo.getText().toString();
         if (messageTo.isEmpty()){
             editTextTo.setError("Field can't be empty");
+         /*
+          * The resulting pattern can then be used to create a Matcher object that can match arbitrary
+          * character sequences against the regular expression.
+          */
+
         }else if (!Patterns.EMAIL_ADDRESS.matcher(messageTo).matches()) {
             editTextTo.setError("Please enter a valid email address");
         }else {
+
+         /*
+          * We can start adding data into the Intent object, we use the method defined in the Intent class putExtra()
+          * Or putExtras() to store certain data as a key value pair or Bundle data object.
+          * These key-value pairs are known as Extras in the sense we are talking about Intents.
+          * Attaching the key value pair using putExtra to this intent
+          */
+
             explicitIntent.putExtra("TO", messageTo);
         }
         String messageSubject = editTextSubject.getText().toString();
+        // Attaching the key value pair using putExtra to this intent
         explicitIntent.putExtra("SUBJECT",messageSubject);
         String messageCompose = editTextCompose.getText().toString();
+        // Attaching the key value pair using putExtra to this intent
         explicitIntent.putExtra("COMPOSE", messageCompose);
         if(messageTo.length()!= 0 &&  messageSubject.length()!= 0 && messageCompose.length()!= 0 ) {
             startActivity(explicitIntent);
@@ -54,4 +71,5 @@ public class explicitActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Please Complete all the fields!", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
